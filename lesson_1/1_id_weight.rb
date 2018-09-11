1d26b33b2fd33b31bc0eba941e2ef25f10d42984
mordecai <malininmo@protonmail.com>
2018-09-11 14:52:39 +0300

    Попытка 2

--- lesson_1/1_id_weight.rb
+++ lesson_1/1_id_weight.rb
@@ -1,15 +1,13 @@
 puts "Ваше имя?"
 name = gets.chomp.capitalize
 
-growth = 0
-
 puts "Ваш рост?"
 growth = gets.to_i
 
 ideal_weight = growth - 110
 
 if ideal_weight <= 0
-    puts "#{name}, Ваш вес уже оптимальный"
+  puts "#{name}, Ваш вес уже оптимальный"
 else
-    puts "#{name}, Ваш идеальный вес #{ideal_weight}"
+  puts "#{name}, Ваш идеальный вес #{ideal_weight}"
 end
--- lesson_1/4_dis.rb
+++ lesson_1/4_dis.rb
@@ -1,14 +1,18 @@
-puts "Введите 3 коэффициента:"
-a = gets.to_i 
-b = gets.to_i
-c = gets.to_i
+puts "Введите 3 коэффициента квадратного уравнения:"
 
-dis = b**2 - (4*a*c)
+a = gets.to_f
+b = gets.to_f
+c = gets.to_f
+
+dis = b**2 - (4 * a * c)
 
 if dis > 0
-     puts "Дискриминант равен #{dis}, корень X1 равен #{(-b) + Math.sqrt(dis) / (a*2)}, корень X2 равен #{(-b) - Math.sqrt(dis) / (a*2)}"
-elsif dis == 0
-    puts "Дискриминант равен #{dis}, корни равны #{(-b) + Math.sqrt(dis) / (a*2)}"
-else 
-    puts "Дискриминант равен #{dis}, корней нет"
+  x1 = (-b) + Math.sqrt(dis) / (a * 2)
+  x2 = (-b) - Math.sqrt(dis) / (a * 2)
+  puts "Дискриминант равен #{dis}, корень X1 равен #{x1}, корень X2 равен #{x2}"
+elsif dis.zero?
+  x = -b / (2 * a)
+  puts "Дискриминант равен 0, корень равен #{x}"
+else
+  puts "Дискриминант равен #{dis}, корней нет"
 end
