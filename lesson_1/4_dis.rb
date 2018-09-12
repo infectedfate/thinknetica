904c54692d46e3345532bfc0237111ae9793435a
mordecai <malininmo@protonmail.com>
2018-09-12 16:03:45 +0300

    попытка 3

--- lesson_1/4_dis.rb
+++ lesson_1/4_dis.rb
@@ -4,15 +4,19 @@ a = gets.to_f
 b = gets.to_f
 c = gets.to_f
 
-dis = b**2 - (4 * a * c)
+dis = b**2 - 4 * a * c
+
+puts "Дискриминант равен #{dis}"
 
 if dis > 0
-  x1 = (-b) + Math.sqrt(dis) / (a * 2)
-  x2 = (-b) - Math.sqrt(dis) / (a * 2)
-  puts "Дискриминант равен #{dis}, корень X1 равен #{x1}, корень X2 равен #{x2}"
+  c = Math.sqrt(dis)
+  x1 = (-b + c) / (2 * a)
+  x2 = (-b - c) / (2 * a)
+  puts "Первый корень равен #{x1}"
+  puts "Второй корень равен #{x2}"
 elsif dis.zero?
   x = -b / (2 * a)
-  puts "Дискриминант равен 0, корень равен #{x}"
+  puts "Корень уравнения равен #{x}"
 else
-  puts "Дискриминант равен #{dis}, корней нет"
+  puts 'Корней нет'
 end
